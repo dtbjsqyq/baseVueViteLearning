06a7814154a4fa3d3cb6740846e2c8df710ac0a2
creat <3102871557@qq.com>
2026-07-13 14:08:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const data = ref(0)
+const parentData = ref("a")
+
+
+
+const increment = () => {
+  data.value++
+  console.log('data.value', data.value)
+}
+
+// 父组件接收&处理参数
+const handleChildAlert = (msg: string) => {
+  alert(msg)
+  parentData.value = msg
+  console.log('父组件接收子组件传递的参数', msg)
+}
+
+const title = ref('你好,PHM!')
+
+const changeTitle = (newTitle: string) => {
+  title.value = newTitle
+}
+
+</script>
+
+<template>
+  <h2>文字1</h2>
+  <button type="button" class="counter" @click="increment">
+    增加 是 {{ data }}
+  </button>
+  <HelloWorld :msg="title" 
+  user="Codex" 
+  @alert-parent="handleChildAlert"
+  @change-title="changeTitle" />
+
+  <h3>展示传来的数据 + {{ parentData }}</h3>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import viteLogo from '../assets/vite.svg'
+import heroImg from '../assets/hero.png'
+import vueLogo from '../assets/vue.svg'
+
+const count = ref(0)
+const countDecrease = ref(0)
+
+const increment = () => {
+  count.value++
+  console.log('count.value', count.value)
+}
+
+
+const props = defineProps<{
+  msg: string,
+  user: string
+}>()
+
+
+// 子传父
+// 声明事件
+const emit = defineEmits(['alert-parent', 'change-title'])
+
+const handleClick = () => {
+  emit('alert-parent', '子组件传递的参数')
+}
+
+const handleChangeTitle = () => {
+  emit('change-title', '新标题')
+}
+
+</script>
+
+<template>
+  <section id="center">
+    <div class="hero">
+      <img :src="heroImg" class="base" width="170" height="179" alt="" />
+      <img :src="vueLogo" class="framework" alt="Vue logo" />
+      <img :src="viteLogo" class="vite" alt="Vite logo" />
+    </div>
+    <div>
+      <h1>Get started</h1>
+      <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
+    </div>
+    <button type="button" class="counter" @click="increment">
+      总数 是 {{ count }}
+    </button>
+    <button type="button" class="counter" @click="countDecrease--">
+      减少 是 {{ countDecrease }}
+    </button>
+
+    <button type="button" class="counter" @click="handleClick">
+      传递参数给父组件
+    </button>
+    <button type="button" class="counter" @click="handleChangeTitle">
+      修改父组件标题
+    </button>
+    <h1>props: {{ props.msg }}</h1>
+    <h1>props: {{ props.user }}</h1>
+
+  </section>
+
+  <div class="ticks"></div>
+
+  <section id="next-steps">
+    <div id="docs">
+      <svg class="icon" role="presentation" aria-hidden="true">
+        <use href="/icons.svg#documentation-icon"></use>
+      </svg>
+      <h2>Documentation</h2>
+      <p>Your questions, answered</p>
+      <ul>
+        <li>
+          <a href="https://vite.dev/" target="_blank">
+            <img class="logo" :src="viteLogo" alt="" />
+            Explore Vite
+          </a>
+        </li>
+        <li>
+          <a href="https://vuejs.org/" target="_blank">
+            <img class="button-icon" :src="vueLogo" alt="" />
+            Learn more
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div id="social">
+      <svg class="icon" role="presentation" aria-hidden="true">
+        <use href="/icons.svg#social-icon"></use>
+      </svg>
+      <h2>Connect with us</h2>
+      <p>Join the Vite community</p>
+      <ul>
+        <li>
+          <a href="https://github.com/vitejs/vite" target="_blank">
+            <svg class="button-icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#github-icon"></use>
+            </svg>
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://chat.vite.dev/" target="_blank">
+            <svg class="button-icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#discord-icon"></use>
+            </svg>
+            Discord
+          </a>
+        </li>
+        <li>
+          <a href="https://x.com/vite_js" target="_blank">
+            <svg class="button-icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#x-icon"></use>
+            </svg>
+            X.com
+          </a>
+        </li>
+        <li>
+          <a href="https://bsky.app/profile/vite.dev" target="_blank">
+            <svg class="button-icon" role="presentation" aria-hidden="true">
+              <use href="/icons.svg#bluesky-icon"></use>
+            </svg>
+            Bluesky
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <div class="ticks"></div>
+  <section id="spacer"></section>
+</template>
